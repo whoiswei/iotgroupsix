dd72b0e381804c9a5eec959923906cff763b6cc8
Who <6309dfes@gmail.com>
2026-06-05 21:33:45 +0800

    Rename project to sixweb

--- sixweb/asgi.py
+++ sixweb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for koweb project.
+ASGI config for sixweb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sixweb.settings')
 
 application = get_asgi_application()
--- sixweb/wsgi.py
+++ sixweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for koweb project.
+WSGI config for sixweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sixweb.settings')
 
 application = get_wsgi_application()
